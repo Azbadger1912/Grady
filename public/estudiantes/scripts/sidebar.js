d12c484a9d9d7d83e8b6ef2203fb446069562028
Fabricio Moreno <fabricio.morenoarauz@gmail.com>
2024-10-31 19:45:34 -0500

    Se volvieron reponsivos los paneles modales de la asignaciones en agenda estudiantes

--- public/estudiantes/scripts/sidebar.js
+++ public/estudiantes/scripts/sidebar.js
@@ -2,9 +2,31 @@
 const sidebar = document.querySelector('.sidebar');
 const toggleBtn = document.querySelector('.toggle-btn');
 
-// Añadir evento de click al boton para mostrar/ocultar el sidebar
+// Añadir evento de click al botón para mostrar/ocultar el sidebar
 toggleBtn.addEventListener('click', () => {
+  if ((window.matchMedia("(min-width: 1140px)").matches) ||  (window.matchMedia("(min-width: 760px)").matches)) {
     sidebar.classList.toggle('active');
+  } else{
+    if (sidebar.classList.contains('active')) {
+        // Primero quitar el ancho con transición
+        sidebar.classList.remove('active');
+        
+        // Luego quitar el z-index después de la transición de ancho
+        setTimeout(() => {
+            sidebar.classList.remove('transition'); 
+            sidebar.classList.remove('z-index-active');
+        }, 500); // Ajusta el tiempo según la duración de la transición de ancho
+    } else {
+        // Primero aplicar el z-index inmediatamente
+        sidebar.classList.add('z-index-active');
+        
+        // Luego aplicar el ancho con transición
+        setTimeout(() => {
+            sidebar.classList.add('transition');
+            sidebar.classList.add('active');
+        }, 200); // Retardo mínimo
+    }
+  }
 });
 
 // Funcion para cerrar la sesion del usuario
